d00b7b26843196c916b30b7e1af77ff35590679b
edtasixm06 <asixm06@edt.org>
2018-11-24 18:57:18 +0100

    homenfs

--- pam/run.sh
+++ pam/run.sh
@@ -2,3 +2,5 @@
 # docker network create ldapnet
 docker run --rm --name ldap -h ldap --net ldapnet -d edtasixm06/ldapserver:18group
 docker run --rm --name host -h host --net ldapnet --privileged -it edtasixm06/hostpam:18homenfs
+docker run --rm --name nfsserver -h nfserver --net ldapnet --privileged -it edtasixm06/nfsserver:18base
+
